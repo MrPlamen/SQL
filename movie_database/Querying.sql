f014b25c761884b38b5b9b8eafd3dcb6510bb81b
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-05-03 00:34:53 +0300

    Update Querying.sql

--- movie_database/Querying.sql
+++ movie_database/Querying.sql
@@ -1,2 +1,9 @@
 SELECT * FROM `countries`
 ORDER BY currency DESC, id;
+
+SELECT CONCAT(first_name,'',last_name) AS `full_name`,
+	CONCAT(REVERSE(last_name),LENGTH(last_name),'@cast.com') AS `email`,
+	YEAR(NOW())-YEAR(birthdate) AS `age`,
+	height
+FROM `actors`
+ORDER BY height ASC
