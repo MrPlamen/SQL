006eb9f0909c5d4c69ee9496d2b1eea323e1c9c9
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-05-03 00:38:59 +0300

    Update Querying.sql

--- movie_database/Querying.sql
+++ movie_database/Querying.sql
@@ -6,4 +6,5 @@ SELECT CONCAT(first_name,'',last_name) AS `full_name`,
 	YEAR(NOW())-YEAR(birthdate) AS `age`,
 	height
 FROM `actors`
+WHERE id NOT IN (SELECT actor_id FROM movies_actors)
 ORDER BY height ASC
